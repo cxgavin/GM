d8198250b5c90a68e786962601f32aa3549bcc98
cxgavin <cxgavin@163.com>
2016-05-20 17:43:53 +0800

    创建

---
+++ 项目管理/项目管理.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace 项目管理.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 项目管理/项目管理/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="项目管理.Global" Language="C#" %>
